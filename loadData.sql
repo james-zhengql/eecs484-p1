848ca26a1495ed12fcabfd81319ad0c1aa63f2a8
Zhengqing Liu <zhengql@caen-vnc-mi10.engin.umich.edu>
2024-09-21 15:35:44 -0400

    check distinct

--- loadData.sql
+++ loadData.sql
@@ -49,49 +49,20 @@ SELECT DISTINCT album_id, owner_id, album_name, album_created_time, album_modifi
 FROM project1.Public_Photo_Information;
 
 INSERT INTO Photos (photo_id, album_id, photo_caption, photo_created_time, photo_modified_time, photo_link)
-SELECT photo_id, album_id, photo_caption, photo_created_time, photo_modified_time, photo_link
+SELECT DISTINCT photo_id, album_id, photo_caption, photo_created_time, photo_modified_time, photo_link
 FROM project1.Public_Photo_Information;
 
 COMMIT;
 SET AUTOCOMMIT ON;
 
 INSERT INTO Tags (tag_photo_id, tag_subject_id, tag_created_time, tag_x, tag_y)
-SELECT photo_id, tag_subject_id, tag_created_time, tag_x_coordinate, tag_y_coordinate
+SELECT DISTINCT photo_id, tag_subject_id, tag_created_time, tag_x_coordinate, tag_y_coordinate
 FROM project1.Public_Tag_Information;
 
-INSERT INTO User_Events (
-    event_id, 
-    event_creator_id, 
-    event_name, 
-    event_tagline, 
-    event_description, 
-    event_host, 
-    event_type, 
-    event_subtype, 
-    event_address, 
-    event_city_id, 
-    event_start_time, 
-    event_end_time
-)
-SELECT
-    E.event_id, 
-    E.event_creator_id, 
-    E.event_name, 
-    E.event_tagline, 
-    E.event_description, 
-    E.event_host, 
-    E.event_type, 
-    E.event_subtype, 
-    E.event_address, 
-    C.city_id, 
-    E.event_start_time, 
-    E.event_end_time
-FROM 
-    project1.Public_Event_Information E
-JOIN 
-    Cities C
-ON 
-    E.event_city = C.city_name
-    AND E.event_state = C.state_name
-    AND E.event_country = C.country_name;
+INSERT INTO User_Events (event_id, event_creator_id, event_name, event_tagline, event_description, event_host, event_type, event_subtype, event_address, event_city_id, event_start_time, event_end_time)
+SELECT DISTINCT E.event_id, E.event_creator_id, E.event_name, E.event_tagline, E.event_description, E.event_host, E.event_type, E.event_subtype, E.event_address, E.event_address, C.city_id, E.event_start_time, E.event_end_time
+FROM project1.Public_Event_Information E, Cities C
+WHERE E.event_city = C.city_name
+AND E.event_state = C.state_name
+AND E.event_country = C.country_name;
 
